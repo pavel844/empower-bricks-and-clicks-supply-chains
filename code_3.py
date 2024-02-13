ad2f1bd51277ebaa374661c56e49ef55d4143478
Pavel <143156330+pavel844@users.noreply.github.com>
2024-02-13 17:48:38 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,16 @@
+        console.log("Random Number: " + item);
+    }
+function generateRandomData() {
+}
+    data.forEach(item => {
+}
+    const data = generateRandomData();
+
+main();
+function main() {
+    for (let i = 0; i < 10; i++) {
+    return data;
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    });
